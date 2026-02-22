6d2e4fa90d00bc11c98e394bba56bb99b4cc2d2e
marrcaburgh <marrcaburgh@gmail.com>
2026-02-22 15:20:17 -0500

    fixed cli test

--- tests/cli.c
+++ tests/cli.c
@@ -3,7 +3,7 @@
 int main(const int argc, const char *argv[]) {
   struct mb_opts app = {.desc = "cli test"};
 
-  if (!mb_opts_init(&app, opts, sizeof(opts[0]))) {
+  if (!mb_opts_init(&app, opts, sizeof(opts) / sizeof(opts[0]))) {
     return 1;
   }
 
