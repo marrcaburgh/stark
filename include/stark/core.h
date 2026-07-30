22f709ff3988dae77ddba792f50624ff4c7376ab
marrcaburgh <marrcaburgh@gmail.com>
2026-07-30 11:01:17 -0400

    add/move/refactor: add; license file for MIT, refactor; move; LICENSE to LICENSES/LGPLv2.1, refactor; libraries to use MIT license

--- include/stark/core.h
+++ include/stark/core.h
@@ -1,21 +1,9 @@
 //
+// SPDX-License-Identifier: MIT
+//
 // stark - a C99+ utility library - stark_core - freestanding compatible
 // compiler intrinsic/builtin macro hub
 // Copyright (C) 2026  marrcaburgh
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 //
 
 #ifndef STARK_CORE_H
